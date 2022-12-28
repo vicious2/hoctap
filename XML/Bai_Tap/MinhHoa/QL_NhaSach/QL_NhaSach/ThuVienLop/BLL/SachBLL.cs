21cb6465383eede39615e1becfca68f031713450
vicious2 <aig.teik@gmail.com>
2022-12-28 16:55:25 +0700

    cap nhat

---
+++ XML/Bai_Tap/MinhHoa/QL_NhaSach/QL_NhaSach/ThuVienLop/BLL/SachBLL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QL_NhaSach.ThuVienLop.DTO;
+using QL_NhaSach.ThuVienLop.DAL;
+
+namespace QL_NhaSach.ThuVienLop.BLL
+{
+    public class SachBLL
+    {
+        public static List<Sach> selectAll()
+        {
+            SachDAL sach_dal = new SachDAL();
+            return sach_dal.selectAll();  
+        }
+
+        public static void Insert(Sach x)
+        {
+            SachDAL sach_dal = new SachDAL();
+            sach_dal.Insert(x);
+        }
+
+        public static void Update(Sach x)
+        {
+            SachDAL sach_dal = new SachDAL();
+            sach_dal.Update(x);
+        }
+
+        public static void Delete(string id)
+        {
+            SachDAL sach_dal = new SachDAL();
+            sach_dal.Delete(id);
+        }
+    }
+}
