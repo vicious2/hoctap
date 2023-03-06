fc611c5952fb16368ab5c2441ccd186583d258e5
vicious2 <aig.teik@gmail.com>
2023-03-06 12:58:26 +0700

    cap nhat

---
+++ Chuong_3/Dataset_Co_dinh_Kieu_monHoc/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dataset_Co_dinh_Kieu_monHoc
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
